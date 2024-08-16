045b8e14b9584de49565ef6daa6d89ea6776b9c1
Renegildo <renegildo@proton.me>
2024-08-16 17:08:48 -0300

    feat: add desktop responsivity in crew page

--- src/components/Crew.tsx
+++ src/components/Crew.tsx
@@ -32,40 +32,48 @@ const Crew = () => {
       }}
     >
       <Navbar />
-      <h3 className="text-xl flex gap-5 font-barlow-condensed tracking-wide w-full justify-center md:mt-10 md:justify-start md:ml-12">
+      <h3 className="text-xl flex gap-5 font-barlow-condensed tracking-wide w-full justify-center md:mt-10 md:justify-start md:ml-12 lg:ml-40">
         <span className="opacity-25">02</span> MEET YOUR CREW
       </h3>
 
-      <div className="flex items-center flex-col gap-2 mt-16 text-xl font-bellefair">
-        <h2 className="opacity-50">{crew[currentCrew].role.toUpperCase()}</h2>
-        <h1 className="text-2xl md:text-4xl">
-          {crew[currentCrew].name.toUpperCase()}
-        </h1>
-      </div>
+      <div className="lg:flex lg:ml-40 lg:gap-8">
+        <div className="flex flex-col lg:h-[65vh] lg:justify-between">
+          <div>
+            <div className="flex items-center lg:items-start flex-col gap-2 mt-16 text-xl font-bellefair">
+              <h2 className="opacity-50">
+                {crew[currentCrew].role.toUpperCase()}
+              </h2>
+              <h1 className="text-2xl md:text-4xl lg:text-[3.5rem] lg:leading-[3.5rem]">
+                {crew[currentCrew].name.toUpperCase()}
+              </h1>
+            </div>
+            <p className="max-w-80 text-center mx-auto mt-6 text-blue-300 md:max-w-lg lg:text-start lg:mx-0">
+              {crew[currentCrew].bio}
+            </p>
+          </div>
 
-      <p className="max-w-80 text-center m-auto mt-6 text-blue-300 md:max-w-lg">
-        {crew[currentCrew].bio}
-      </p>
-
-      <div className="flex gap-4 w-full justify-center mt-20 z-10">
-        {[...new Array(4)].map((_, i) => (
-          <button
-            key={i}
-            onClick={() => setCurrentCrew(i)}
-            className={`rounded-full bg-white w-3 h-3 ${i === currentCrew ? "opacity-100" : "opacity-25"} transition`}
+          <div className="flex gap-4 w-full justify-center lg:justify-start mt-20 z-10">
+            {[...new Array(4)].map((_, i) => (
+              <button
+                key={i}
+                onClick={() => setCurrentCrew(i)}
+                className={`rounded-full bg-white w-3 h-3 ${i === currentCrew ? "opacity-100" : "opacity-25"} transition`}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="relative mt-8 z-10">
+          <img
+            className="max-lg:w-1/2 m-auto pointer-events-none select-none lg:h-full lg:w-full"
+            src={crew[currentCrew].images.png}
+            alt={crew[currentCrew].name}
+            width={350}
+            height={470}
           />
-        ))}
-      </div>
-
-      <div className="relative mt-8 z-10">
-        <img
-          className="w-1/2 m-auto pointer-events-none select-none"
-          src={crew[currentCrew].images.png}
-          alt={crew[currentCrew].name}
-          width={350}
-          height={470}
-        />
-        <div className="absolute bg-gradient-to-b left-0 bottom-0 w-full h-64 from-transparent to-blue-900" />
+          <div
+            className={`absolute bg-gradient-to-b left-0 bottom-0 w-full h-64 from-transparent to-blue-900 ${currentCrew === 3 ? "h-52" : ""}`}
+          />
+        </div>
       </div>
     </section>
   );
