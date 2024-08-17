70a5c908367d654cf12a2bdb2e189dd96b60cc96
Renegildo <renegildo@proton.me>
2024-08-17 11:05:18 -0300

    feat: add responsive design to technology section

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -33,38 +33,46 @@ const Technology = () => {
     >
       <Navbar />
 
-      <h2 className="font-barlow-condensed text-xl flex w-full justify-center gap-6 mb-10">
+      <h2 className="font-barlow-condensed text-xl flex w-full justify-center gap-6 mb-10 md:mt-10 md:justify-start md:ml-10 lg:ml-[10rem] lg:text-3xl">
         <span className="opacity-25">03</span> SPACE LAUNCH 101
       </h2>
 
-      <div className="flex flex-col">
-        <div className="relative w-full md:max-w-screen-sm h-72 overflow-hidden">
+      <div className="flex flex-col lg:flex-row-reverse lg:justify-center lg:items-center">
+        <div className="relative w-full lg:max-w-screen-sm h-72 overflow-hidden lg:h-[36.5rem] lg:w-full">
           <img
-            className="absolute object-cover w-full -translate-y-40"
-            src={technology[currentTech].images.portrait}
+            className="absolute object-cover w-full"
+            src={
+              width < 1024
+                ? technology[currentTech].images.landscape
+                : technology[currentTech].images.portrait
+            }
           />
         </div>
         <div>
-          <div className="flex gap-4 my-[2.5rem] w-full justify-center">
-            {technology.map((_, i) => (
-              <button
-                onClick={() => setCurrentTech(i)}
-                className={`rounded-full w-10 h-10 transition-colors ${currentTech === i ? "bg-white border border-white text-black" : "border border-white/25"}`}
-              >
-                {i + 1}
-              </button>
-            ))}
-          </div>
+          <div className="lg:flex">
+            <div className="flex gap-4 my-[2.5rem] w-full justify-center lg:flex-col lg:w-fit lg:my-0">
+              {technology.map((_, i) => (
+                <button
+                  onClick={() => setCurrentTech(i)}
+                  className={`rounded-full w-10 h-10 md:w-14 md:h-14 lg:w-20 lg:h-20 transition-colors font-bellefair md:text-xl ${currentTech === i ? "bg-white border border-white text-black" : "border border-white/25"}`}
+                >
+                  {i + 1}
+                </button>
+              ))}
+            </div>
 
-          <h2 className="text-center opacity-50 font-bellefair text-lg">
-            THE TERMINOLOGY
-          </h2>
-          <h1 className="text-center mb-5 mt-2 text-2xl font-bellefair">
-            {technology[currentTech].name.toUpperCase()}
-          </h1>
-          <p className="text-center text-blue-300 max-w-96 leading-[180%] m-auto">
-            {technology[currentTech].description}
-          </p>
+            <div className="lg:mx-16">
+              <h2 className="text-center opacity-50 font-bellefair text-lg md:text-2xl lg:text-start">
+                THE TERMINOLOGY...
+              </h2>
+              <h1 className="text-center mb-5 mt-2 text-2xl font-bellefair md:text-4xl lg:text-start lg:text-5xl">
+                {technology[currentTech].name.toUpperCase()}
+              </h1>
+              <p className="text-center text-blue-300 max-w-96 leading-[180%] m-auto md:max-w-screen-sm lg:text-start">
+                {technology[currentTech].description}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
